ff8aa5d21a22316a49882e6294f84aee2b655832
KJCM <KJCM@KEFT>
2022-06-28 17:20:40 -0500

    Segunda subida del proyecto1

--- H4Github/src/h4github/H4Github.java
+++ H4Github/src/h4github/H4Github.java
@@ -5,6 +5,10 @@ public class H4Github {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        int x=4;
+        int y=5;
+        int suma=x+y;
+        System.out.println("la suma:"+suma);
     }
     
 }
